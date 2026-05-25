b248a26e0a10794b7cf80ceb132d321f2cdf7d04
Johana-bou <johanaboundin90@gmail.com>
2026-05-25 15:29:50 +0200

    Update main.tsx

--- douanes-app/src/main.tsx
+++ douanes-app/src/main.tsx
@@ -1,6 +1,6 @@
 import React from 'react'
 import ReactDOM from 'react-dom/client'
-import { BrowserRouter } from 'react-router-dom'
+import { HashRouter } from 'react-router-dom'   // ← Changement ici
 import { QueryClient, QueryClientProvider } from '@tanstack/react-query'
 import { Toaster } from 'react-hot-toast'
 import './index.css'
@@ -15,10 +15,10 @@ const queryClient = new QueryClient({
 ReactDOM.createRoot(document.getElementById('root')!).render(
   <React.StrictMode>
     <QueryClientProvider client={queryClient}>
-      <BrowserRouter>
+      <HashRouter>                               {/* ← Changement ici */}
         <App />
         <Toaster position="top-right" toastOptions={{ duration: 4000 }} />
-      </BrowserRouter>
+      </HashRouter>
     </QueryClientProvider>
   </React.StrictMode>,
 )
